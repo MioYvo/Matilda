bc2c316c61ac2a476eb01e03d69e17a1f7d9f635
mio <liurusi.101@gmail.com>
2019-01-14 06:30:34 +0800

    Delete build_push.sh
    delete unused files

--- build_push.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-sudo docker build --rm -t matilda .
-sudo docker tag matilda:latest ccr.ccs.tencentyun.com/mioo/matilda:latest
-sudo docker push ccr.ccs.tencentyun.com/mioo/matilda:latest
